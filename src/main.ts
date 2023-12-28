54e300df302283a7a179726a72a68ce1c6705a08
petro647 <petrosinoa096@gmail.com>
2023-12-28 15:30:41 +0100

    autocomplete

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+interface ObjOfElements {
+  [key: string]: Element
+}
+
 /**
  * Singleton Global class
 */
@@ -9,6 +13,8 @@ class Global {
     this.state = _stateToAdd;
   }
 
+  static elements: ObjOfElements = {};
+
   static getInstance() {
     if (!this.instance) {
       this.instance = new Global();
@@ -18,6 +24,7 @@ class Global {
 }
 
 const container: HTMLElement | null = document.querySelector('.container');
+const backendURL = "http://localhost:3000";
 
 function main(_container: HTMLElement | null){
   if(!container){
@@ -75,6 +82,37 @@ function _clickHandler(e: MouseEvent){
 
     showHide.classList.toggle('show-password');
   }
+
+  if(target.matches('.login-page button.login')){
+    const loginBox = document.querySelector('.login-page .login-box');
+    const userNameInput: HTMLInputElement | null | undefined = loginBox?.querySelector('input[name="username"]');
+    const passwordInput: HTMLInputElement | null | undefined = loginBox?.querySelector('input[name="password"]');
+    if(!userNameInput || !passwordInput){
+      return;
+    }
+    const userName = userNameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    fetch(`${backendURL}/login/signin?user_name=${userName}&password=${password}`)
+    .then(res => {
+      const status = res.status;
+      console.log(status);
+
+      // todo: implementare il fatto che il backend mi debba restituire un token quando mi loggo, e io solo con quel token posso fare le richieste successive
+      // invece lato backend devo assicurarmi che dopo 5 tentativi sbagliati della password non sia possibile piu loggarsi con quell utente, a quel punto verra inviato un codice di conferma via sms da dover inserire nell app per confermare il login
+
+      if(status != 200){
+        throw new Error;
+      }
+      return res.json()
+    })
+    .then(json => {
+      console.log(json);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 }
 
 function _inputHandler(e: Event){
@@ -89,6 +127,10 @@ function _inputHandler(e: Event){
       return;
     }
     showHide.classList.toggle('show-img', true);
+  }
+
+  if(target.matches('input.login[name="username"]')){
+
   }
 }
 
